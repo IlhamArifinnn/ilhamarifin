2f5259965f87462f0442b6c325666ce7b548bd99
IlhamArifinnn <arifinn2208@gmail.com>
2026-01-07 09:45:37 +0700

    refactor: create file skills

---
+++ src/utils/data/skills.js
@@ -0,0 +1,75 @@
+export const skills = [
+  {
+    name: "HTML",
+    icon: "/logos/html5.svg",
+  },
+  {
+    name: "CSS",
+    icon: "/logos/css3.svg",
+  },
+  {
+    name: "JavaScript",
+    icon: "/logos/javascript.svg",
+  },
+  {
+    name: "PHP",
+    icon: "/logos/php.svg",
+  },
+  {
+    name: "React",
+    icon: "/logos/react.svg",
+  },
+  {
+    name: "Laravel",
+    icon: "/logos/laravel.svg",
+  },
+  {
+    name: "Python",
+    icon: "/logos/python.svg",
+  },
+  {
+    name: "Node.js",
+    icon: "/logos/nodejs.svg",
+  },
+  {
+    name: "Express.js",
+    icon: "/logos/express.svg",
+  },
+  {
+    name: "MySQL",
+    icon: "/logos/mysql.svg",
+  },
+  {
+    name: "Tailwind",
+    icon: "/logos/tailwindcss.svg",
+  },
+  {
+    name: "Bootstrap",
+    icon: "/logos/bootstrap.svg",
+  },
+  {
+    name: "Git",
+    icon: "/logos/git.svg",
+  },
+
+  {
+    name: "Github",
+    icon: "/logos/github.svg",
+  },
+  {
+    name: "Figma",
+    icon: "/logos/figma.svg",
+  },
+  {
+    name: "Vercel",
+    icon: "/logos/vercel.svg",
+  },
+  {
+    name: "AWS Basic",
+    icon: "/logos/aws.svg",
+  },
+  {
+    name: "Azure",
+    icon: "/logos/azure.svg",
+  },
+];
